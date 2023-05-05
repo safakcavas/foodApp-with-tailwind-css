2e4c4b796bb24e024afb39a8a38624b68dee888e
safakcavas <safakcvs@gmail.com>
2023-05-05 20:53:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-300 w-screen h-screen">
+      <h1 className="bg-slate-100 p-2 m-2 mt-1 underline" >Food App</h1> 
     </div>
   );
 }
