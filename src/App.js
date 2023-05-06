59f174dc72d9feb8f3d92881c996f916ddbc1428
safakcavas <safakcvs@gmail.com>
2023-05-06 22:29:16 +0300

    Map Data and Top Rated Menu added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HeadlineCards from './components/HeadlineCards';
+import Food from "./components/Food";
 function App() {
   return (
     <div className="bg-gray-300 w-screen h-screen"> 
       <Navbar/>
       <Hero/>
       <HeadlineCards/>
+      <Food/>
     </div>
   );
 }
